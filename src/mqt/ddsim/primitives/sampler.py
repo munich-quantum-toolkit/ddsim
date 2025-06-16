84a9fe496d739172fe29b6ba42b7af1b899c7948
Daniel Haag <denialhaag@gmail.com>
2025-06-16 12:24:53 +0200

    Use CircuitSimulator directly in Sampler

--- src/mqt/ddsim/primitives/sampler.py
+++ src/mqt/ddsim/primitives/sampler.py
@@ -13,6 +13,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import numpy as np
+from mqt.core import load
+from qiskit.circuit import QuantumCircuit
 from qiskit.primitives.base import BaseSamplerV2
 from qiskit.primitives.containers import (
     BitArray,
@@ -24,7 +26,7 @@ from qiskit.primitives.containers import (
 from qiskit.primitives.containers.sampler_pub import SamplerPub
 from qiskit.primitives.primitive_job import PrimitiveJob
 
-from mqt.ddsim.qasmsimulator import QasmSimulatorBackend
+from mqt.ddsim.pyddsim import CircuitSimulator
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -36,21 +38,24 @@ class Sampler(BaseSamplerV2):  # type: ignore[misc]
     The implementation is adapted from Qiskit's `StatevectorSampler`.
     """
 
-    _BACKEND = QasmSimulatorBackend()
-
-    def __init__(self, *, default_shots: int = 1024) -> None:
+    def __init__(self, *, default_shots: int = 1024, seed: np.random.Generator | int | None = -1) -> None:
         """Create a new DDSIM sampler."""
         self._default_shots = default_shots
 
-    @property
-    def backend(self) -> QasmSimulatorBackend:
-        """The backend used by the sampler."""
-        return self._BACKEND
+        if not isinstance(seed, int):
+            msg = "seed must be an int."
+            raise TypeError(msg)
+        self._seed = seed
 
     @property
     def default_shots(self) -> int:
         """Return the default shots."""
         return self._default_shots
+
+    @property
+    def seed(self) -> int:
+        """Return the seed."""
+        return self._seed
 
     def run(
         self,
@@ -81,14 +86,25 @@ class Sampler(BaseSamplerV2):  # type: ignore[misc]
 
         bound_circuits = parameter_values.bind_all(circuit)
         bound_circuits_list = np.asarray(bound_circuits, dtype=object).tolist()
-        result = self.backend.run(bound_circuits_list, shots=shots).result()
+        if isinstance(bound_circuits_list, QuantumCircuit):
+            bound_circuits_list = [bound_circuits_list]
 
-        counts = result.get_counts()
-        if isinstance(counts, dict):
-            counts = [counts]
+        counts = self._run_experiment(bound_circuits_list, shots=shots, seed=self.seed)
 
         meas = {creg.name: BitArray.from_counts(counts, creg.size) for creg in circuit.cregs}
         return SamplerPubResult(
             DataBin(**meas, shape=pub.shape),
             metadata={"shots": shots, "circuit_metadata": circuit.metadata},
         )
+
+    @staticmethod
+    def _run_experiment(quantum_circuits: list[QuantumCircuit], shots: int, seed: int) -> list[dict[str, int]]:
+        counts_list = []
+
+        for quantum_circuit in quantum_circuits:
+            loaded_quantum_circuit = load(quantum_circuit)
+            simulator = CircuitSimulator(loaded_quantum_circuit, seed=seed)
+            counts = simulator.simulate(shots)
+            counts_list.append(counts)
+
+        return counts_list
